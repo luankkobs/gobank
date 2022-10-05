1f25a48cdb5695378b410b6a2d5be95a20b77a22
Luan Kobs <luankkobs@gmail.com>
2022-10-05 16:03:32 -0300

    Implementing billet payment

--- main.go
+++ main.go
@@ -6,12 +6,27 @@ import (
 	"github.com/luankkobs/bank/accounts"
 )
 
+func PayBillet(account verifyAccount, valueOfBillet float64) {
+	account.Withdrawal(valueOfBillet)
+}
+
+type verifyAccount interface {
+	Withdrawal(value float64) string
+}
+
 func main() {
 	denisAccount := accounts.SavingsAccount{}
 
 	denisAccount.Deposit(300)
-	denisAccount.Withdrawal(200)
+	PayBillet(&denisAccount, 60)
 
 	fmt.Println(denisAccount.GetBalance())
+
+	luisaAccount := accounts.CheckingAccount{}
+	luisaAccount.Deposit(500)
+
+	PayBillet(&luisaAccount, 1000)
+
+	fmt.Println(luisaAccount.GetBalance())
 
 }
